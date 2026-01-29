43740de44b9713fb4490e86d821f361c42a37b9e
DGoblot <90686224+DGoblot@users.noreply.github.com>
2026-01-29 16:54:21 +0100

    added trampoline and plat remapping

--- scripts/PLAT/playerPLAT.gd
+++ scripts/PLAT/playerPLAT.gd
@@ -12,7 +12,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	if Input.is_action_just_pressed(&"Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		_jump()
 	var direction := Input.get_axis(&"Left", &"Right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -22,12 +22,21 @@ func _physics_process(delta: float) -> void:
 		get_tree().change_scene_to_file("res://scenes/DRIVE/DRIVE.tscn")
 
 	move_and_slide()
+	
+func _jump() -> void:
+	velocity.y = JUMP_VELOCITY
 
 
 func _on_death_zone_body_entered(body: Node2D) -> void:
-	print_debug("dead")
-	get_tree().change_scene_to_file("res://scenes/PLAT/PLAT.tscn")
+	if body.name == "PlayerPLAT":
+		get_tree().change_scene_to_file("res://scenes/PLAT/PLAT.tscn")
 
 
 func _on_finish_area_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_file("res://scenes/DRIVE/DRIVE.tscn")
+	if body.name == "PlayerPLAT":
+		get_tree().change_scene_to_file("res://scenes/DRIVE/DRIVE.tscn")
+
+
+func _on_trampoline_detector_area_entered(area: Area2D) -> void:
+	if area.name == "TrampArea":
+		_jump()
